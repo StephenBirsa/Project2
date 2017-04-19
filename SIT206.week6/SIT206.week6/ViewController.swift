2d010ebc5166200ceb482c499dd53e0f7605a024
STEPHEN BIRSA <sbirsa@10-150-45-159.dhcp-m.deakin.edu.au>
2017-04-20 08:52:11 +1000

    2nd check

--- SIT206.week6/SIT206.week6/ViewController.swift
+++ SIT206.week6/SIT206.week6/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //test comment 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
